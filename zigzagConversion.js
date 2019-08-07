d50515594c9090ffcbc22c30fb091b51d0e0da08
Kim Tran <mykim.tran297@gmail.com>
2019-08-06 23:30:51 -0700

    Medium

---
+++ zigzagConversion.js
@@ -0,0 +1,49 @@
+/*
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+*/
+var convert = function(s, numRows) {
+    let result = '';
+    if (numRows === 1 || s.length <=1) {
+        return s;
+    }
+    
+    const matrix = new Array(numRows);
+    for (let i = 0; i <= numRows; i++) {
+        matrix[i] = [];
+    }
+    
+    for (let j = 0; j < s.length; j++) {
+        let col = 2 * numRows - 2;
+        let position = j % col;
+        let row = position < numRows ? position : col - position;
+        matrix[row].push(s[j])
+    }
+    
+    return matrix.map(r => r.join('')).join('');
+};
+
+//Time complexity: O(n);
+//Space complexity: O(n)
